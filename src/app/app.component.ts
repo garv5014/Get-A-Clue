226a3cbb0b90f9246cb713b81d75780054721c67
garrett vernon <gvernon@dhicorp.com>
2024-05-15 14:26:49 -0600

    it is working now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,38 @@
-import { AfterViewInit, Component, NgModule, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
 import { ClueStorageService } from './services/clue-storage.service';
+import { gameObject } from './models/gameObject.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule],
+  imports: [RouterOutlet, MatToolbarModule, CommonModule, MatCardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit, AfterViewInit {
   title = 'Get-A-Clue';
 
-  gameState = {};
+  gameState: gameObject = {
+    Weapons: [],
+    Locations: [],
+    Suspects: [],
+  };
   constructor(private _clueStorageService: ClueStorageService) {}
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
     this._clueStorageService.initStorage();
+    this.gameState = this._clueStorageService.getClueState();
+    console.log(this.gameState);
   }
-  
+
   ngAfterViewInit(): void {
     //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
     //Add 'implements AfterViewInit' to the class.
-    this.gameState = this._clueStorageService.getClueState();
   }
 }
