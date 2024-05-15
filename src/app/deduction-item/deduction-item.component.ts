a01a3a6fd6a81d9414f631de7fae8ac5c17a51db
garrett vernon <gvernon@dhicorp.com>
2024-05-15 16:08:50 -0600

    working on the drop downs

--- src/app/deduction-item/deduction-item.component.ts
+++ src/app/deduction-item/deduction-item.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { DeductionStatus } from '../models/deductionStatus.enum';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-deduction-item',
   standalone: true,
-  imports: [],
+  imports: [MatSelectModule],
   templateUrl: './deduction-item.component.html',
-  styleUrl: './deduction-item.component.scss'
+  styleUrl: './deduction-item.component.scss',
 })
 export class DeductionItemComponent {
+  constructor() {}
 
+  @Input({
+    required: true,
+  })
+  currentStatus: DeductionStatus = DeductionStatus.Unmarked;
+
+  @Input({
+    required: true,
+  })
+  updateStatus!: (newStatus: DeductionStatus) => void;
 }
