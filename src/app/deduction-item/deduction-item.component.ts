ca2789afd449d5192c2cbe0ce53aae412e05f5ee
garrett vernon <gvernon@dhicorp.com>
2024-05-15 15:25:29 -0600

    scss sucks

---
+++ src/app/deduction-item/deduction-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-deduction-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './deduction-item.component.html',
+  styleUrl: './deduction-item.component.scss'
+})
+export class DeductionItemComponent {
+
+}
