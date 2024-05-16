27feed55166e0d50f62dba955ecb85849333a8a0
garrett vernon <gvernon@dhicorp.com>
2024-05-16 12:55:15 -0600

    colors

--- src/app/deduction-item/deduction-item.component.ts
+++ src/app/deduction-item/deduction-item.component.ts
@@ -1,10 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { DeductionStatus } from '../models/deductionStatus.enum';
 import { MatSelectModule } from '@angular/material/select';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormsModule } from '@angular/forms';
 
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { ThemePalette } from '@angular/material/core';
 
 @Component({
   selector: 'app-deduction-item',
@@ -45,8 +46,25 @@ export class DeductionItemComponent {
     newStatus: DeductionStatus;
   }>();
 
+  selectorControl = new FormControl('primary' as ThemePalette);
+
+  getColor(newStatus: DeductionStatus) {
+    switch (newStatus) {
+      case DeductionStatus.NotSeen:
+        return 'primary' as ThemePalette;
+      case DeductionStatus.Possible:
+        return 'accent' as ThemePalette;
+      case DeductionStatus.RuledOut:
+        return 'warn' as ThemePalette;
+      default:
+        break;
+    }
+    return 'primary' as ThemePalette;
+  }
+  formColor = this.getColor(this.currentStatus);
   selectionChange(event: any) {
     console.log(event);
+    this.formColor = this.getColor(event.value);
     this.statusUpdate.emit({
       itemName: this.name,
       itemCategory: this.category,
