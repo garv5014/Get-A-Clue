b15e45cc3e6d856c19ba0f97442beb17f0e03c49
garrett vernon <gvernon@dhicorp.com>
2024-05-16 10:20:52 -0600

    Auto-generated commit

--- src/app/models/gameObject.interface.ts
+++
@@ -1,7 +0,0 @@
-import { DeductionItem } from './deductionItems.interface';
-
-export interface gameObject {
-  Weapons: Array<DeductionItem>;
-  Locations: Array<DeductionItem>;
-  Suspects: Array<DeductionItem>;
-}
